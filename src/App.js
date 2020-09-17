9901ee13f897b958e579ac2c6403f07ad1ce94ea
Octomo63 <6135512019@psu.ac.th>
2020-09-17 19:58:02 +0700

    Final_CheckpointLab

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-//check1
-// function App() {
-//   return (
-//     // <div>
-//     //   Hello World {1+5}
-//     // </div>
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <p>
-//     //       Edit <code>src/App.js</code> and save to reload.
-//     //     </p>
-//     //     <a
-//     //       className="App-link"
-//     //       href="https://reactjs.org"
-//     //       target="_blank"
-//     //       rel="noopener noreferrer"
-//     //     >
-//     //       Learn React
-//     //     </a>
-//     //   </header>
-//     // </div>
-//   );
-// }
-
-//check2
-    // import CharacterCard from './CharacterCard';
-    import WordCard from './WordCard';
+import WordCard from './WordCard';
     const word = "Hello"
     function App() {
     return (
       <div>
       {
-        // Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
         <WordCard value="hello"/>
       }
       </div>
@@ -42,4 +14,3 @@ import './App.css';
     }
 
 export default App;
-//test
